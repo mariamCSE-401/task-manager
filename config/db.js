a39b65212e79d6901df59f0135b75fac5e5dccdf
mariamCSE-401 <20220655007mariam@juniv.edu>
2025-11-21 18:55:43 +0600

    Add MySQL integration

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'taskuser',
+  password: 'taskpass123',
+  database: 'taskdb',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = connection.promise();
